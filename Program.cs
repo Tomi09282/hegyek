932e1cb6135cd712cc767e55db0e9ec29a6ada89
Tomi09282 <112693387+Tomi09282@users.noreply.github.com>
2023-05-18 14:38:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿namespace MoHegyei
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Atlag();
+            LegmagasabbHegy();
+            Borzsony();
+            Magasabb();
+        }
+
+
+        static List<hegycsucs> csucsok = new List<hegycsucs>();
+        static List<double> magasok = new List<double>();
+        static void Beolvas()
+        {
+            StreamReader read = new StreamReader("hegyek.txt");
+            read.ReadLine();
+            while (!read.EndOfStream)
+            {
+                string[] adat = read.ReadLine().Split(';');
+                csucsok.Add(new hegycsucs(adat[0], adat[1], int.Parse(adat[2])));
+            }
+            read.Close();
+            Console.WriteLine($"Hegycsúcsok száma: {csucsok.Count} db");
+        }
+
+        static void Atlag()
+        {
+            double avg = 0;
+            double sum = 0;
+            foreach (var i in csucsok)
+            {
+                sum = sum + i.Magassag;
+            }
+            avg = sum / csucsok.Count;
+            Console.WriteLine($"átlag: {avg}");
+        }
+
+
+        static void LegmagasabbHegy()
+        {
+            string neve = "";
+            string hegyseg = "";
+            int magassag = 0;
+            int max = 0;
+            foreach (var i in csucsok)
+            {
+                if (max < i.Magassag)
+                {
+                    max = i.Magassag;
+                    neve = i.Hegycsúcs;
+                    hegyseg = i.Hegyseg;
+                    magassag = i.Magassag;
+                }
+            }
+            Console.WriteLine($"A legmagasabb hegycsúcs:");
+            Console.WriteLine($"neve: {neve}" + $"\nhegyseg: {hegyseg}" + $"\n magassag: {magassag} m");
+        }
+
+
+        static void Borzsony()
+        {
+            Console.Write("magassag: ");
+            int rl = int.Parse(Console.ReadLine());
+            int eredmeny = 0;
+            foreach (var i in csucsok)
+            {
+                if (i.Hegyseg.Contains("Börzsöny"))
+                {
+                    if (rl < i.Magassag)
+                    {
+                        eredmeny = i.Magassag;
+                        Console.WriteLine($"Van magasabb hegycsúcs a Börzsönyben {rl}-nél, {eredmeny}.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{rl}m-nél nincs magasabb hegycsucs.");
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        static void Magasabb()
+        {
+
+            double szam = 3.280839895;
+            int count = 0;
+            foreach (var i in csucsok)
+            {
+                magasok.Add(i.Magassag * szam);
+            }
+            foreach (var i in magasok)
+            {
+                if (i > 3000)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"3000 nél magasabb hegyek száma: {count}");
+
+        }
+
+
+
+    }
+}
---
+++ hegycsucs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoHegyei
+{
+    class hegycsucs
+    {
+        private string hegycsúcs;
+
+        public string Hegycsúcs
+        {
+            get { return hegycsúcs; }
+        }
+
+        private string hegyseg;
+
+        public string Hegyseg { get { return hegyseg; } }
+
+        private int magassag;
+
+        public int Magassag { get { return magassag; } }
+
+        public hegycsucs(string hegycsúcs, string hegyseg, int magassag)
+        {
+            this.hegycsúcs = hegycsúcs;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+
+    }
+}
